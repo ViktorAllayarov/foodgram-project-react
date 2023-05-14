558bd37708dd6fbe6b88f46dc827ef4ff4c28124
Viktor Oblomov <viktorall88@gmail.com.com>
2023-05-15 00:07:06 +0300

    fix workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 AUTH_USER_MODEL = "users.User"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
